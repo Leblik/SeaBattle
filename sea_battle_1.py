31e75a0455a1811e7351dcacfdf8f69b00ff3cd2
Alexey <leblik87@gmail.com>
2021-11-20 03:26:47 +0300

    Version 0.3:  + rename "player 1" to "player" and "player 2" to "computer", because we don't have a multiplayer yet =)

--- sea_battle_1.py
+++ sea_battle_1.py
@@ -8,7 +8,7 @@
 # хранения резульатов выстрелов.
 
 print("\n  Sea Battle Game \n ver.0.1")
-# Make fields of fleets for player 1 and player 2.
+# Make fields of fleets for Player 1 and Computer.
 # Ships in every fleet: x5 - 1, x4 - 1, x3 - 1, x2 - 2, x1 - 2
 # 1 var - by hand
 fleet_pl_1 = [[0, 0, 0, 0, 0, 1, 0, 0, 1, 1],
@@ -22,7 +22,7 @@ fleet_pl_1 = [[0, 0, 0, 0, 0, 1, 0, 0, 1, 1],
               [0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
               [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-fleet_pl_2 = [[0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+fleet_comp = [[0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 1, 0, 0, 1, 1, 0, 0],
               [0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
               [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
@@ -35,11 +35,11 @@ fleet_pl_2 = [[0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
 
 # Change int elmnts to str in fleets
 fleet_pl_1 = [[str(i) for i in row] for row in fleet_pl_1]
-fleet_pl_2 = [[str(i) for i in row] for row in fleet_pl_2]
+fleet_comp = [[str(i) for i in row] for row in fleet_comp]
 
-# Make fields of shots for player 1 and player 2.
+# Make fields of shots for Player 1 and Computer.
 shots_pl_1 = [['~' for i in range(10)] for row in range(10)]
-shots_pl_2 = [['~' for i in range(10)] for row in range(10)]
+shots_comp = [['~' for i in range(10)] for row in range(10)]
 
 
 # Function print_fields. Print 2 fields in one line
@@ -51,20 +51,22 @@ def print_fields(fleet_1_name, fleet_2_name, fleet_1, fleet_2):
         print(y, "   ", ' '.join([str(elem) for elem in ship_1]), " " * 12, y,"  ", ' '.join([str(elem) for elem in ship_2]))
     x_axis = ' '.join([str(x) for x in axis])
     print("    X", x_axis, " " * 16, x_axis)   # print x_axis
+    # TODO: make print with format
 
 
 # Game loop
 run = True
 while run:
-    # print_fields("Player 1 fleet", "Player 2 fleet", fleet_pl_1, fleet_pl_2)    # show players fleets
-    print_fields("Player 1 shots", "Player 2 shots", shots_pl_1, shots_pl_2)    # show players shots
+    print_fields("Player 1 fleet", "Computer fleet", fleet_pl_1, fleet_comp)    # show players fleets
+    print_fields("Player 1 shots", "Computer shots", shots_pl_1, shots_comp)    # show players shots
 
-#Player 1 make shot
+# Player 1 make shot
 # shot_pl_1 = input("Enter player 1 shot coordinate:").split()
-    shot = input("\n Player 1 shot to X Y axis of Player 2:")
+    shot = input("\n Enter Player 1 shot coordinate X Y to Computer:")
+# TODO: - add to shots code of player and computer prohibition on repeat of coordinates
 
 # quit the game
-    if shot == 'q' or shot == 'q':
+    if shot == 'q':
         run = False
         print("\n", " " * 16, "Exit the game")
         exit()
@@ -76,19 +78,19 @@ while run:
 
     print("Player 1 shot in x1, y1 = ", x1, "-", y1)  # debug
 
-    if fleet_pl_2[y1][x1] == '1':  # y1 - num of column, x1 - num of elem in string
-        print("Yehaaa! Ship is hit =)")
-        fleet_pl_2[y1][x1] = 'X'
+    if fleet_comp[y1][x1] == '1':  # y1 - num of column, x1 - num of elem in string
+        print("Yehaaa! Ship is hit!")
+        fleet_comp[y1][x1] = 'X'
         shots_pl_1[y1][x1] = 'X'
-    elif fleet_pl_2[y1][x1] == '0':
+    elif fleet_comp[y1][x1] == '0':
         print("You miss... ")
-        fleet_pl_2[y1][x1] = '.'
+        fleet_comp[y1][x1] = '.'
         shots_pl_1[y1][x1] = '.'
 
 
 # Print result of fire
-# print_fields("Player 1 fleet", "Player 2 fleet", fleet_pl_1, fleet_pl_2)    # show player fleets
-#     print_fields("Player 1 shots", "Player 2 shots", shots_pl_1, shots_pl_2)    # show player shots
+# print_fields("Player 1 fleet", "Player 2 fleet", fleet_pl_1, fleet_comp)    # show player fleets
+#     print_fields("Player 1 shots", "Player 2 shots", shots_pl_1, shots_comp)    # show player shots
 
 # quit()
 # exit()
